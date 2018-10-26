246f37e74350069288ce2c4326975ff60623b765
kevin leòn <kroa@QUADIONTECH.COM>
2018-10-26 17:34:56 -0300

    creé la vista para agregar recitales

---
+++ tutorial/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using tutorial.Models;
+using tutorial.ViewModels;
+
+namespace tutorial.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
