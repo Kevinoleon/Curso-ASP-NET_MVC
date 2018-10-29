5ffdd7c2677bc7a7ffbe73c669f3d1fc1f94a7f1
kevin leòn <kroa@QUADIONTECH.COM>
2018-10-29 15:24:42 -0300

    agregé llaves foraneas de genero y artista a la tabla de Gigs y además de esto, y ahora la acción crear está completamente implementada, ya se pueden agregar registros a la BD ( aún no hay validación en los formularios)

--- tutorial/Controllers/GigsController.cs
+++ tutorial/Controllers/GigsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
 using System.Web.Mvc;
 using tutorial.Models;
 using tutorial.ViewModels;
@@ -7,7 +9,7 @@ namespace tutorial.Controllers
 {
     public class GigsController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
@@ -23,5 +25,25 @@ namespace tutorial.Controllers
             };
             return View(viewModel);
         }
+
+        [Authorize]
+        [HttpPost  ]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+              
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
